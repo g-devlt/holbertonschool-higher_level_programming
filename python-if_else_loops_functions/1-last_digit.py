7672310116831e45e0247aa623d99840f9193aed
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-16 13:58:39 +0000

    Added T1

---
+++ python-if_else_loops_functions/1-last_digit.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import random
+number = random.randint(-10000, 10000)
+
+ld = number % 10
+end = ""
+
+if (ld > 5):
+    end = "and is greater than 5"
+elif (ld == 0):
+    end = "and is zero"
+elif (ld < 6):
+    end = "and is less than 6 and not 0"
+
+print(f'The last digit of {number} is {ld} {end}')
