186689825e04dca627e19cb2d30ec5588d031627
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-22 10:04:27 +0000

    Undid

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,12 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    if my_list is None: 
-        return None
-    if idx < 0 or idx >= len(my_list):
-        return my_list.copy()
-    new_list = my_list.copy()
-    new_list[idx] = element
-    return new_list
+    if(my_list is None):
+        return
+    return [element if idx == i else x for (i, x) in enumerate(my_list.copy())]
 
 
 if __name__ == "__main__":
