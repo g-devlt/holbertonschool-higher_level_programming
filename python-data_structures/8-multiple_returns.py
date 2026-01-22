fa9bb61c8d1c4f7d87593b60902f2b07e8535ef8
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-22 12:38:52 +0000

    None checking

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
+    if sentence is None:
+        return 0, None
     if len(sentence) == 0:
         return None
     return len(sentence), sentence[0]
