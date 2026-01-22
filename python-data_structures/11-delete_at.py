df549619d3315ab14509c0a2fa7c630050b7f5fa
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-22 12:47:55 +0000

    Added T11

---
+++ python-data_structures/11-delete_at.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def delete_at(my_list=[], idx=0):
+    if my_list is None:
+        return None
+    out = list()
+    for i, x in enumerate(my_list):
+        if(idx != i)
+            out.append(x)
