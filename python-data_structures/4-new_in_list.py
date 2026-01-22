0fb7129b2cb28a71373de5c7db7203e2cc85f5ec
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-22 09:59:07 +0000

    Fix T4

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
-    return [element if(idx == i) else x for (i, x) in enumerate(my_list)]
+    if(my_list is None):
+        return
+    return [element if idx == i else x for (i, x) in enumerate(my_list.copy())]
 
 
 if __name__ == "__main__":
