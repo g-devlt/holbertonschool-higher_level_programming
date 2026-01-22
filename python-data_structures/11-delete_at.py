0691523e8662955bf676d7ee8465937a6ac7aba1
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-22 12:55:18 +0000

    Fixed style

--- python-data_structures/11-delete_at.py
+++ python-data_structures/11-delete_at.py
@@ -4,9 +4,10 @@ def delete_at(my_list=[], idx=0):
         return None
     out = list()
     for i, x in enumerate(my_list):
-        if(i != idx):
+        if i != idx:
             out.append(x)
     return out
+
 
 if __name__ == "__main__":
     print(delete_at([1, 2, 3, 4], 2))
