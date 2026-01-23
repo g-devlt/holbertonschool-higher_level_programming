38dae7077a46650e1d408d8aca7aaa620a7be22f
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-23 13:47:34 +0000

    Fixed T0

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -3,10 +3,7 @@ def safe_print_list(my_list=[], x=0):
     i = 0
     while i < x:
         try:
-            if i > 0:
-                print(" {}".format(my_list[i]), end = "")
-            else:
-                print("{}".format(my_list[i]), end = "")
+            print(my_list[i], end="")
             i += 1
         except IndexError:
             print("")
