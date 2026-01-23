d7d63534c01cb3fbc561b0117243fc3561b69d3b
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-23 12:58:13 +0000

    Added T10

---
+++ python-more_data_structures/10-best_score.py
@@ -0,0 +1,10 @@
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    return sorted(a_dictionary.items())[-1][0]
+
+
+if __name__ == "__main__":
+    a_dictionary = {'John': 12, 'Bob': 14, 'Mike': 14, 'Molly': 16, 'Adam': 10}
+    best_key = best_score(a_dictionary)
+    print("Best score: {}".format(best_key))
