5b2b32b8b400b2dc56b956af96637a83c97cd255
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-23 13:42:38 +0000

    Added T0

---
+++ python-exceptions/0-safe_print_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    i = 0
+    while i < x:
+        try:
+            print(my_list[i])
+            i += 1
+        except IndexError:
+            return i
+
+
+if __name__ == "__main__":
+    print(safe_print_list([1, 2, 3], 4))
