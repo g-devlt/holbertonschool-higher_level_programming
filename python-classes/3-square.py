e0b40c53125af59fb9bb0f06d78d7ecdf8b4eb16
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-30 13:41:11 +0000

    Added T3

---
+++ python-classes/3-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""A simple module for a basic Square class
+"""
+
+
+class Square:
+    """A basic Square class
+    """
+    def __init__(self, size=0):
+        """Initialization for Square instances.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        return self.__size ** 2
+
+
+if __name__ == "__main__":
+    my_square_1 = Square(3)
+    print("Area: {}".format(my_square_1.area()))
+
+    try:
+        print(my_square_1.size)
+    except Exception as e:
+        print(e)
+
+    try:
+        print(my_square_1.__size)
+    except Exception as e:
+        print(e)
+
+    my_square_2 = Square(5)
+    print("Area: {}".format(my_square_2.area()))
