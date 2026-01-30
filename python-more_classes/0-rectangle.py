f801c7b8c0972a57d6f17f5050924e8494489345
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-01-30 14:19:18 +0000

    Added T0

---
+++ python-more_classes/0-rectangle.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""A simple module for a basic Rectangle class
+"""
+
+class Rectangle:
+    """A basic Rectangle class
+    """
+
+
+if __name__ == "__main__":
+    my_rectangle = Rectangle()
+    print(type(my_rectangle))
+    print(my_rectangle.__dict__)
