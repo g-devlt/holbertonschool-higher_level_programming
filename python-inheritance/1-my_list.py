c64e99ba7ddcea5b9ed3e7e0dd3fdb8ca5ee397e
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-02-02 13:45:02 +0000

    Added T1

---
+++ python-inheritance/1-my_list.py
@@ -0,0 +1,19 @@
+"""A simple module for a sorting list.
+"""
+
+class MyList(list):
+    """A class that inherits from list.
+    """
+
+    def print_sorted(self):
+        """A function that allows to print a sorted version of this Object"""
+        print(sorted(self))
+
+
+if __name__ == "__main__":
+    mylist = MyList()
+    mylist.append(2)
+    mylist.append(3)
+    mylist.append(99)
+    mylist.append(1)
+    mylist.print_sorted()
