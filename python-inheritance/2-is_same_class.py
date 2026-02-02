3c451f0e8c7cfb377e4f6d8b710961ebe4bbf67a
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-02-02 14:34:59 +0000

    Added T2

---
+++ python-inheritance/2-is_same_class.py
@@ -0,0 +1,22 @@
+"""A module for is_same_class
+"""
+
+
+class A():
+    """Testing base class"""
+    pass
+
+
+class B(A):
+    """Testing child class"""
+    pass
+
+
+def is_same_class(obj, a_class):
+    """Returns wether obj is of the class 'a_class'"""
+    return type(obj).__name__ == a_class.__name__
+
+
+if __name__ == "__main__":
+    print(is_same_class(B(), A))
+    print(is_same_class(1, int))
