04cc7fe18f9f1fe8aaa42afbd578f64bcfb2f255
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-02-05 08:59:17 +0000

    Fixed style T3

--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -2,6 +2,10 @@
 """A module that holds class comparisons functions
 """
 
-def is_kind_of_class(obj, a_class : type):
-    """Returns True if the object is an instance of a class or a subclass of a_class"""
+
+def is_kind_of_class(obj, a_class: type):
+    """
+    Returns True if the object is an
+    instance of a class or a subclass of a_class
+    """
     return isinstance(obj, a_class)
