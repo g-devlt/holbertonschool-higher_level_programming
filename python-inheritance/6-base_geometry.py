17218d9687b5152747c03e2b428c42c30975f253
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-02-06 14:25:39 +0000

    Added T6

---
+++ python-inheritance/6-base_geometry.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""A module for BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """BaseGeometry class
+    A class to handle geometry
+    """
+    
+    def area(self):
+        """Returns or prints the area of the object
+        """
+        raise Exception("area() is not implemented")
