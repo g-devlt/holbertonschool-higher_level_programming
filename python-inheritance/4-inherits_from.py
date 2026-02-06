b1860bdf8592b6c39ba4cc00b17d0dd1c8b7c355
g-devlt <gabriel.devasselot.de.regne@gmail.com>
2026-02-06 14:21:40 +0000

    Fixed T4 Added T5

--- python-inheritance/4-inherits_from.py
+++ python-inheritance/4-inherits_from.py
@@ -4,6 +4,18 @@
 
 
 def inherits_from(obj, a_class):
+    """A function that checks wether an object
+    is an instance of a subclass of a_class
+    """
     a = isinstance(obj, a_class)
-    b = not (type(obj).__name__ == a_class.__name__)
-    return (a and b)
+    b = (type(obj).__name__ == a_class.__name__)
+    return (a and not b)
+
+if __name__ == "__main__":
+    a = True
+    if inherits_from(a, int):
+        print("{} inherited class {}".format(a, int.__name__))
+    if inherits_from(a, bool):
+        print("{} inherited class {}".format(a, bool.__name__))
+    if inherits_from(a, object):
+        print("{} inherited class {}".format(a, object.__name__))
---
+++ python-inheritance/5-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""A module for BaseGeometry
+"""
+
+
+class BaseGeometry():
+    """BaseGeometry class
+    A class to handle geometry
+    """
+    pass
